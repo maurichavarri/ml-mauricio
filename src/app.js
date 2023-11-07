00eddff751cbb5a413984258af152d21cc855441
maurichavarri <mauriciochavarri@gmail.com>
2023-11-07 15:09:38 -0300

    creando .env

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 3001;
 
 app.use(express.static(path.resolve(__dirname, '../public')));
 
-app.listen(3000, () => {
-    console.log("Servidor corriendo.");
-});
+app.listen (port, () => console.log(`Servidor funcionando bien en puerto ${port}`));
 
 app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
+
+
+
